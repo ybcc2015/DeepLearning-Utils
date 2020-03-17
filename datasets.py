81e99d9e31c9a20edbc91a01ae6187cdeda65bc0
ybcc <271380178@qq.com>
2020-03-17 14:06:25 +0800

    add parse_csv() func

--- datasets.py
+++ datasets.py
@@ -3,6 +3,7 @@ import numpy as np
 import glob
 import os
 import json
+import cv2
 
 
 def parse_xml(annot_dir):
@@ -36,7 +37,7 @@ def parse_xml(annot_dir):
 
 def parse_json(annot_dir):
     """
-    Parse json annotation files from labelme.
+    Parse json annotation files.
 
     :param annot_dir: directory path to annotation files
     :return: 2-d array
@@ -61,5 +62,31 @@ def parse_json(annot_dir):
             h_norm = (ymax - ymin) / h_img
 
             boxes.append([w_norm, h_norm])
+
+    return np.array(boxes)
+
+
+def parse_csv(file_path):
+    """
+    Parse csv annotation file.
+
+    :param file_path: path of the csv file
+    :return: 2-d array
+    """
+    boxes = []
+
+    with open(file_path) as f:
+        lines = f.readlines()
+
+    for line in lines:
+        items = line.strip().split(',')
+        img = cv2.imread(items[0])
+        h_img, w_img = img.shape[:2]
+        xmin, ymin, xmax, ymax = list(map(int, items[1:-1]))
+
+        w_norm = (xmax - xmin) / w_img
+        h_norm = (ymax - ymin) / h_img
+
+        boxes.append([w_norm, h_norm])
 
     return np.array(boxes)
