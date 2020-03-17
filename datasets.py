e86c4cb237f7ec20641b83dc7f9bb5d5b8b5d5cc
ybcc <271380178@qq.com>
2020-03-17 15:09:25 +0800

    refactor parse funs into AnnotParse cls

--- datasets.py
+++ datasets.py
@@ -6,87 +6,106 @@ import json
 import cv2
 
 
-def parse_xml(annot_dir):
-    """
-    Parse XML annotation files in VOC dataset.
+class AnnotParse(object):
+    def __init__(self, file_type):
+        assert file_type in ['csv', 'xml', 'json'], "Unsupported file type."
+        self.file_type = file_type
 
-    :param annot_dir: directory path to annotation files
-    :return: 2-d array
-    """
-    boxes = []
+    def parse(self, path):
+        """
 
-    for xml_file in glob.glob(os.path.join(annot_dir, '*.xml')):
-        tree = ET.parse(xml_file)
+        :param path: directory path to annotation files
+        :return: 2-d array
+        """
+        if self.file_type == 'xml':
+            return self.parse_xml(path)
+        elif self.file_type == 'json':
+            return self.parse_json(path)
+        else:
+            return self.parse_csv(path)
 
-        h_img = int(tree.findtext('./size/height'))
-        w_img = int(tree.findtext('./size/width'))
+    @staticmethod
+    def parse_xml(annot_dir):
+        """
+        Parse XML annotation files in VOC dataset.
 
-        for obj in tree.iter('object'):
-            xmin = int(round(float(obj.findtext('bndbox/xmin'))))
-            ymin = int(round(float(obj.findtext('bndbox/ymin'))))
-            xmax = int(round(float(obj.findtext('bndbox/xmax'))))
-            ymax = int(round(float(obj.findtext('bndbox/ymax'))))
+        :param annot_dir: directory path to annotation files
+        :return: 2-d array
+        """
+        boxes = []
+
+        for xml_file in glob.glob(os.path.join(annot_dir, '*.xml')):
+            tree = ET.parse(xml_file)
+
+            h_img = int(tree.findtext('./size/height'))
+            w_img = int(tree.findtext('./size/width'))
+
+            for obj in tree.iter('object'):
+                xmin = int(round(float(obj.findtext('bndbox/xmin'))))
+                ymin = int(round(float(obj.findtext('bndbox/ymin'))))
+                xmax = int(round(float(obj.findtext('bndbox/xmax'))))
+                ymax = int(round(float(obj.findtext('bndbox/ymax'))))
+
+                w_norm = (xmax - xmin) / w_img
+                h_norm = (ymax - ymin) / h_img
+
+                boxes.append([w_norm, h_norm])
+
+        return np.array(boxes)
+
+    @staticmethod
+    def parse_json(annot_dir):
+        """
+        Parse json annotation files.
+
+        :param annot_dir: directory path to annotation files
+        :return: 2-d array
+        """
+        boxes = []
+
+        for js_file in glob.glob(os.path.join(annot_dir, '*.json')):
+            with open(js_file) as f:
+                data = json.load(f)
+
+            h_img = data['imageHeight']
+            w_img = data['imageWidth']
+
+            for shape in data['shapes']:
+                points = shape['points']
+                xmin = int(round(points[0][0]))
+                ymin = int(round(points[0][1]))
+                xmax = int(round(points[1][0]))
+                ymax = int(round(points[1][1]))
+
+                w_norm = (xmax - xmin) / w_img
+                h_norm = (ymax - ymin) / h_img
+
+                boxes.append([w_norm, h_norm])
+
+        return np.array(boxes)
+
+    @staticmethod
+    def parse_csv(file_path):
+        """
+        Parse csv annotation file.
+
+        :param file_path: path of the csv file
+        :return: 2-d array
+        """
+        boxes = []
+
+        with open(file_path) as f:
+            lines = f.readlines()
+
+        for line in lines:
+            items = line.strip().split(',')
+            img = cv2.imread(items[0])
+            h_img, w_img = img.shape[:2]
+            xmin, ymin, xmax, ymax = list(map(int, items[1:-1]))
 
             w_norm = (xmax - xmin) / w_img
             h_norm = (ymax - ymin) / h_img
 
             boxes.append([w_norm, h_norm])
 
-    return np.array(boxes)
-
-
-def parse_json(annot_dir):
-    """
-    Parse json annotation files.
-
-    :param annot_dir: directory path to annotation files
-    :return: 2-d array
-    """
-    boxes = []
-
-    for js_file in glob.glob(os.path.join(annot_dir, '*.json')):
-        with open(js_file) as f:
-            data = json.load(f)
-
-        h_img = data['imageHeight']
-        w_img = data['imageWidth']
-
-        for shape in data['shapes']:
-            points = shape['points']
-            xmin = int(round(points[0][0]))
-            ymin = int(round(points[0][1]))
-            xmax = int(round(points[1][0]))
-            ymax = int(round(points[1][1]))
-
-            w_norm = (xmax - xmin) / w_img
-            h_norm = (ymax - ymin) / h_img
-
-            boxes.append([w_norm, h_norm])
-
-    return np.array(boxes)
-
-
-def parse_csv(file_path):
-    """
-    Parse csv annotation file.
-
-    :param file_path: path of the csv file
-    :return: 2-d array
-    """
-    boxes = []
-
-    with open(file_path) as f:
-        lines = f.readlines()
-
-    for line in lines:
-        items = line.strip().split(',')
-        img = cv2.imread(items[0])
-        h_img, w_img = img.shape[:2]
-        xmin, ymin, xmax, ymax = list(map(int, items[1:-1]))
-
-        w_norm = (xmax - xmin) / w_img
-        h_norm = (ymax - ymin) / h_img
-
-        boxes.append([w_norm, h_norm])
-
-    return np.array(boxes)
+        return np.array(boxes)
